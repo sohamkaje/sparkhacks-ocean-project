125942f049272b8d5a71bb7b93089e8fa6254280
Vasu Patel <vpate217@uic.edu>
2025-02-07 23:00:30 -0600

    added style to login page

--- sparkhacks-ocean-project/src/pages/LoginPage/login.jsx
+++ sparkhacks-ocean-project/src/pages/LoginPage/login.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
+import "./login.css"; // Ensure this CSS file exists and is properly linked
 
 const Login = () => {
   return (
     <div className="login-container">
       <div className="login-box">
+        <h1 className="title">EcoTide</h1>
         <h2>Login</h2>
         <p>Sign in to continue</p>
         <button className="google-login">
